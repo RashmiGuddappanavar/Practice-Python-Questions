f798f35107b2aa9dbf1962e43b68349488154ddd
Rashmi Guddappanavar <140267494+RashmiGuddappanavar@users.noreply.github.com>
2025-08-09 23:12:11 +0530

    Add files via upload

---
+++ Strings/VowelsInString.py
@@ -0,0 +1,9 @@
+# Count the number of vowels in a string
+
+vowels = "aeiouAEIOU"
+str1 = input("Enter a String: ")
+count = 0
+for chr in str1:
+    if chr in str1:
+        count += 1
+print(count)        
